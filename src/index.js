4b13d57bffe390c0ada5e30df32c01d40f5198e4
Kate <kate.matveeva@gmail.com>
2021-03-13 17:52:16 +0300

    feat: add emplimentation for towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if ( matrix === undefined)
+    return [];
+    let towel = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            towel = towel.concat(matrix[i])
+
+        } else {
+            towel = towel.concat(matrix[i].reverse())
+        }
+
+    }
+    return towel
 }
